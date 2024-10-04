a46d4cb97f2e4161530e30ec582190cb64c230d1
Yaroslav Lysak <lysak.yaroslav00@yandex.ru>
2024-10-04 22:34:56 +0300

    [v.0.0.1] initial commit

---
+++ cmd/entities/targets/queue.go
@@ -0,0 +1,156 @@
+package targets
+
+import (
+	_ "embed"
+	"errors"
+	"log/slog"
+	"net"
+	"net/netip"
+	"sync"
+)
+
+type ScanTargetQueue struct {
+	Targets []*ScanTarget
+
+	addForked   bool
+	useReserved bool
+
+	sync.Mutex
+}
+
+// NewScanTargetQueue creates new queue for a scanner, automatically generates targets with type from string slice.
+// AllowForking defines if domain or ip should be extracted from host.
+// AllowReserved defines if queue should contain reserved IP addresses.
+func NewScanTargetQueue(from []string, allowForking, allowReserved bool) (*ScanTargetQueue, error) {
+	if len(from) == 0 {
+		return nil, errors.New("empty target list")
+	}
+
+	var t_ *ScanTarget
+	var err error
+
+	queue := new(ScanTargetQueue)
+	queue.Targets = make([]*ScanTarget, 0, len(from))
+	queue.addForked = allowForking
+	queue.useReserved = allowReserved
+
+	for _, t := range from {
+		t_, err = NewScanTarget(AutoTypeScanTarget(t, queue.addForked))
+		if err != nil {
+			break
+		}
+
+		queue.Targets = append(queue.Targets, t_)
+	}
+
+	if err != nil {
+		return queue, errors.New("failed to create scan queue: " + err.Error())
+	}
+
+	return queue, nil
+}
+
+func (queue *ScanTargetQueue) Enqueue(from []string, allowForking bool) error {
+	queue.Lock()
+
+	if len(from) == 0 {
+		return errors.New("empty target list")
+	}
+
+	var t_ *ScanTarget
+	var err error
+
+	targets_ := make([]*ScanTarget, 0, len(from))
+
+	for _, t := range from {
+		t_, err = NewScanTarget(AutoTypeScanTarget(t, allowForking))
+		if err != nil {
+			break
+		}
+
+		targets_ = append(targets_, t_)
+	}
+
+	queue.Targets = append(queue.Targets, targets_...)
+	queue.Unlock()
+	return nil
+}
+
+func (queue *ScanTargetQueue) Output(outputChan chan string) {
+	queue.Lock()
+
+	for _, t := range queue.Targets {
+		if t.IPNet != nil {
+			p, err := netip.ParsePrefix(t.IPNet.String())
+			if err != nil {
+				slog.Error("failed to parse target ip net: " + err.Error())
+				continue
+			}
+
+			p = p.Masked()
+			addr := p.Addr()
+
+			if addr.AsSlice()[3] == 0 {
+				addr = addr.Next()
+			}
+
+			for {
+				if !p.Contains(addr) {
+					break
+				}
+
+				outputChan <- addr.String()
+				addr = addr.Next()
+			}
+
+			t.IPNet = nil
+		}
+
+		if t.Domain != nil {
+			outputChan <- *t.Domain
+			t.Domain = nil
+		}
+
+		if t.Mailbox != nil {
+			outputChan <- t.Mailbox.Address
+			t.Mailbox = nil
+		}
+
+		if t.URL != nil {
+			outputChan <- t.URL.String()
+			t.URL = nil
+		}
+	}
+
+	queue.Unlock()
+	close(outputChan)
+}
+
+func (queue *ScanTargetQueue) produceIPs(network net.IPNet, outputChan chan string, allowReserved bool) error {
+	p, err := netip.ParsePrefix(network.String())
+	if err != nil {
+		return errors.New("failed to parse target ip net: " + err.Error())
+	}
+
+	p = p.Masked()
+	addr := p.Addr()
+
+	if addr.AsSlice()[3] == 0 {
+		addr = addr.Next()
+	}
+
+	if allowReserved {
+		for {
+			if !p.Contains(addr) {
+				break
+			}
+
+			outputChan <- addr.String()
+			addr = addr.Next()
+		}
+	} else {
+
+	}
+
+	return nil
+}
---
+++ test/targets_test.go
@@ -0,0 +1,404 @@
+package test
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"gitlab.qvineox.ru/domsnail/threat-intel-core/cmd/entities/targets"
+	"net"
+	"testing"
+)
+
+func TestTargets(t *testing.T) {
+	t.Run("ip network reservation checks", func(t *testing.T) {
+		hosts := []string{
+			"192.0.2.1",
+			"192.0.2.2",
+			"233.252.0.1",
+			"240.0.0.2",
+			"255.255.255.255",
+			"0.0.0.1",
+			"127.12.1.2",
+			"128.12.1.1",
+			"91.122.10.3",
+			"91.12.1.1",
+			"52.2.1.22",
+		}
+
+		require.True(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[0])))
+		require.True(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[1])))
+		require.True(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[2])))
+		require.True(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[3])))
+		require.True(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[4])))
+		require.True(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[5])))
+		require.True(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[6])))
+
+		require.False(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[7])))
+		require.False(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[8])))
+		require.False(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[9])))
+		require.False(t, targets.CheckIsIPv4Reserved(net.ParseIP(hosts[10])))
+	})
+
+	t.Run("scan target queue creation with domains only", func(t *testing.T) {
+		hosts := []string{
+			"yandex.ru",
+			"ya.ru",
+			"mail.ya.ru",
+			"mai41sl.fya.r1-u",
+			"уцаьзфып.вп.r1-u",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 5)
+	})
+
+	t.Run("scan target queue creation with incorrect domains", func(t *testing.T) {
+		hosts := []string{
+			"yandex..ru",
+			"y!a.ru",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.Error(t, err)
+		require.Empty(t, queue.Targets)
+	})
+
+	t.Run("scan target queue creation with CIDRs only", func(t *testing.T) {
+		hosts := []string{
+			"10.10.10.0/32",
+			"212.121.122.0/16",
+			"2001:4860:4860::8888/32",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 3)
+	})
+
+	t.Run("scan target queue creation with incorrect CIDRs", func(t *testing.T) {
+		hosts := []string{
+			"256.10.10.0/32",
+			"212.121.122.0",
+			"2001:4860:4860::8888",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.Error(t, err)
+		require.Empty(t, queue.Targets)
+	})
+
+	t.Run("scan target queue creation with URLs only", func(t *testing.T) {
+		hosts := []string{
+			"https://yandex.ru",
+			"http://mail.ru",
+			"https://mail.yandex.ru",
+			"https://user:pass@mail.yandex.ru",
+			"//mail.yandex.ru",
+			"ftp://ftp.yandex.ru",
+			"smb://smb.yandex.ru",
+			"https://10.212.12.21/test",
+			"https://2001:4860:4860::8888/test",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 9)
+	})
+
+	t.Run("scan target queue creation with URLs forking", func(t *testing.T) {
+		hosts := []string{
+			"https://yandex.ru",
+			"http://mail.ru",
+			"https://mail.yandex.ru",
+			"https://user:pass@mail.yandex.ru",
+			"//mail.yandex.ru",
+			"ftp://ftp.yandex.ru",
+			"smb://smb.yandex.ru",
+			"https://10.212.12.21/test",
+			"https://2001:4860:4860::8888/test",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, true, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 9)
+
+		require.NotNil(t, queue.Targets[0].URL)
+		require.NotNil(t, queue.Targets[0].Domain)
+
+		require.NotNil(t, queue.Targets[3].URL)
+		require.NotNil(t, queue.Targets[3].Domain)
+
+		require.NotNil(t, queue.Targets[7].URL)
+		require.NotNil(t, queue.Targets[7].IPNet)
+	})
+
+	t.Run("scan target queue creation with incorrect URLs", func(t *testing.T) {
+		hosts := []string{
+			"https:/\\mail.yandex.ru",
+			"/mail.yandex.ru",
+			"/pass@mail.yandex.ru",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.Error(t, err)
+		require.Empty(t, queue.Targets)
+	})
+
+	t.Run("scan target queue creation with mailboxes only", func(t *testing.T) {
+		hosts := []string{
+			"yarlrusman@gmail.com",
+			"yarlrusman@gmail.mail.com",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 2)
+	})
+
+	t.Run("scan target queue creation with incorrect mailboxes", func(t *testing.T) {
+		hosts := []string{
+			"test@@mail.ru",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.Error(t, err)
+		require.Empty(t, queue.Targets)
+	})
+
+	t.Run("scan target queue creation with mailboxes forking", func(t *testing.T) {
+		hosts := []string{
+			"yarlrusman@gmail.com",
+			"yarlrusman@yandex.mail.ru",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, true, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 2)
+
+		require.NotNil(t, queue.Targets[0].Mailbox)
+		require.NotNil(t, queue.Targets[0].Domain)
+
+		require.NotNil(t, queue.Targets[1].Mailbox)
+		require.NotNil(t, queue.Targets[1].Domain)
+	})
+
+	t.Run("scan target queue creation with mixed hosts", func(t *testing.T) {
+		hosts := []string{
+			"https://yandex.ru",
+			"212.122.32.12/32",
+			"ya.ru",
+			"yarlrusman@yandex.ru",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 4)
+
+		require.NotNil(t, queue.Targets[0].URL)
+		require.NotNil(t, queue.Targets[1].IPNet)
+		require.NotNil(t, queue.Targets[2].Domain)
+		require.NotNil(t, queue.Targets[3].Mailbox)
+	})
+
+	t.Run("scan target queue creation with mixed hosts and errors", func(t *testing.T) {
+		hosts := []string{
+			"https://yandex.ru",
+			"212.122.32.12",
+			"ya.ru",
+			"/mail.yandex.ru",
+			"212.121.122.0",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, false, false)
+		require.Error(t, err)
+		require.Len(t, queue.Targets, 1)
+
+		hosts = []string{
+			"https://yandex.ru",
+			"ya.ru",
+			"/mail.yandex.ru",
+			"212.122.32.12",
+			"212.121.122.0",
+		}
+
+		queue, err = targets.NewScanTargetQueue(hosts, false, false)
+		require.Error(t, err)
+		require.Len(t, queue.Targets, 2)
+	})
+
+	t.Run("scan target queue expansion", func(t *testing.T) {
+		hosts := []string{
+			"https://yandex.ru",
+			"https://212.122.32.36/path",
+			"212.122.32.12/32",
+			"ya.ru",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, true, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 4)
+
+		hosts = []string{
+			"https://ya.ru",
+			"212.122.32.0/16",
+		}
+
+		err = queue.Enqueue(hosts, true)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 6)
+	})
+
+	t.Run("scan target queue stream output", func(t *testing.T) {
+		hosts := []string{
+			"https://yandex.ru",
+			"https://212.122.32.36/path",
+			"212.122.32.12/32",
+			"ya.ru",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, true, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 4)
+
+		outputChan := make(chan string, 100)
+
+		ctx := context.Background()
+		go queue.Output(outputChan)
+
+		var t_ = make([]string, 0)
+
+		for {
+			target, ok := <-outputChan
+			if ok {
+				t_ = append(t_, target)
+			} else {
+				require.NoError(t, ctx.Err())
+				break
+			}
+		}
+
+		require.Len(t, t_, 6)
+		require.Equal(t, t_[0], "yandex.ru")
+		require.Equal(t, t_[1], "https://yandex.ru")
+		require.Equal(t, t_[2], "212.122.32.36")
+		require.Equal(t, t_[3], "https://212.122.32.36/path")
+		require.Equal(t, t_[4], "212.122.32.12")
+		require.Equal(t, t_[5], "ya.ru")
+	})
+
+	t.Run("scan target queue stream output with subnet ignore checks, subnet 30", func(t *testing.T) {
+		hosts := []string{
+			"212.122.32.0/30",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, true, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 1)
+
+		outputChan := make(chan string, 100)
+
+		ctx := context.Background()
+		go queue.Output(outputChan)
+
+		var t_ = make([]string, 0)
+
+		for {
+			target, ok := <-outputChan
+			if ok {
+				t_ = append(t_, target)
+			} else {
+				require.NoError(t, ctx.Err())
+				break
+			}
+		}
+
+		require.Len(t, t_, 3)
+	})
+
+	t.Run("scan target queue stream output with subnet ignore checks, subnet 24", func(t *testing.T) {
+		hosts := []string{
+			"212.122.32.0/24",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, true, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 1)
+
+		outputChan := make(chan string)
+
+		ctx := context.Background()
+		go queue.Output(outputChan)
+
+		var t_ = make([]string, 0)
+
+		for {
+			target, ok := <-outputChan
+			if ok {
+				t_ = append(t_, target)
+			} else {
+				require.NoError(t, ctx.Err())
+				break
+			}
+		}
+
+		require.Len(t, t_, 255)
+	})
+
+	t.Run("scan target queue stream output with subnet unmasking, subnet 16", func(t *testing.T) {
+		hosts := []string{
+			"212.132.0.0/16",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, true, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 1)
+
+		outputChan := make(chan string, 100)
+
+		ctx := context.Background()
+		go queue.Output(outputChan)
+
+		var t_ = make([]string, 0)
+
+		for {
+			target, ok := <-outputChan
+			if ok {
+				t_ = append(t_, target)
+			} else {
+				require.NoError(t, ctx.Err())
+				break
+			}
+		}
+
+		require.Len(t, t_, 65535)
+	})
+
+	t.Run("scan target queue stream output with subnet unmasking, subnet 8", func(t *testing.T) {
+		hosts := []string{
+			"212.0.0.0/8",
+		}
+
+		queue, err := targets.NewScanTargetQueue(hosts, true, false)
+		require.NoError(t, err)
+		require.Len(t, queue.Targets, 1)
+
+		outputChan := make(chan string, 1000)
+
+		ctx := context.Background()
+		go queue.Output(outputChan)
+
+		var t_ = make([]string, 0)
+
+		for {
+			target, ok := <-outputChan
+			if ok {
+				t_ = append(t_, target)
+			} else {
+				require.NoError(t, ctx.Err())
+				break
+			}
+		}
+
+		require.Len(t, t_, 16777215)
+	})
+}
