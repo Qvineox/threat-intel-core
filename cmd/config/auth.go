13bc3d57e5d4db6aab678668275a8e83f2681b1c
Yaroslav Lysak <lysak.yaroslav00@yandex.ru>
2025-02-10 21:19:06 +0300

    minor config changes

--- cmd/config/auth.go
+++ cmd/config/auth.go
@@ -4,11 +4,14 @@ import "os"
 
 type Auth struct {
 	Token  string
+	Domain string
 	Secret []byte
 }
 
 func NewAuthConfigFromEnv() (config Auth) {
 	config.Token = os.Getenv("AUTH_TOKEN")
+	config.Domain = os.Getenv("AUTH_TOKEN_DOMAIN")
+
 	config.Secret = []byte(os.Getenv("AUTH_TOKEN_SECRET"))
 
 	return
