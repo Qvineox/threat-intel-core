05a1390b959333931cbd480d598310a0abdadbbe
Yaroslav Lysak <lysak.yaroslav00@yandex.ru>
2025-02-26 21:54:51 +0300

    updated scheduled routine entity

---
+++ cmd/entities/timekeeping/routine.go
@@ -0,0 +1,55 @@
+package timekeeping
+
+import (
+	"github.com/google/uuid"
+	"gitlab.domsnail.ru/domsnail/threat-intel-core/api/services"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"time"
+)
+
+type ScheduledRoutine struct {
+	UUID uuid.UUID `json:"UUID"`
+
+	IsEnabled bool `json:"IsEnabled"`
+	IsRunning bool `json:"IsRunning"`
+
+	Name        string `json:"Name"`
+	Group       string `json:"Group"`
+	Description string `json:"Description"`
+
+	Cron string `json:"Cron"`
+
+	LastSuccessText  string `json:"LastSuccessText"`
+	LastRunErrorText string `json:"LastRunErrorText"`
+
+	LastRunAt           *time.Time `json:"LastRunAt"`
+	LastSuccessfulRunAt *time.Time `json:"LastSuccessfulRunAt"`
+	NextRunAt           time.Time  `json:"NextRunAt"`
+
+	Run *func() error `json:"-"`
+}
+
+func (r ScheduledRoutine) ToProto() *services.ScheduledRoutine {
+	p := &services.ScheduledRoutine{
+		UUID:             r.UUID.String(),
+		IsEnabled:        r.IsEnabled,
+		IsRunning:        r.IsRunning,
+		Name:             r.Name,
+		Group:            r.Group,
+		Description:      r.Description,
+		CRON:             r.Cron,
+		NextRunAt:        timestamppb.New(r.NextRunAt),
+		LastSuccessText:  r.LastSuccessText,
+		LastRunErrorText: r.LastRunErrorText,
+	}
+
+	if r.LastRunAt != nil {
+		p.LastRunAt = timestamppb.New(*r.LastRunAt)
+	}
+
+	if r.LastSuccessfulRunAt != nil {
+		p.LastSuccessfulRunAt = timestamppb.New(*r.LastSuccessfulRunAt)
+	}
+
+	return p
+}
