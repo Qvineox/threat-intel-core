a86c3eb6cef5134ed619e76053ab79c6609f4b51
Yaroslav Lysak <lysak.yaroslav00@yandex.ru>
2025-03-12 20:19:15 +0300

    updated auth service config triggers

--- cmd/config/services.go
+++ cmd/config/services.go
@@ -9,6 +9,7 @@ type Services struct {
 	CoordinatorURL      string
 	ControlCenterURL    string
 	CollectorURL        string
+	AuthURL             string
 	TelegramAdminBotURL string
 }
 
@@ -16,6 +17,7 @@ func NewServicesConfigFromEnv() (config Services) {
 	config.ControlCenterURL = os.Getenv("CONTROL_CENTER_URL")
 	config.CoordinatorURL = os.Getenv("COORDINATOR_URL")
 	config.CollectorURL = os.Getenv("COLLECTOR_URL")
+	config.AuthURL = os.Getenv("AUTH_URL")
 	config.TelegramAdminBotURL = os.Getenv("TG_ADMIN_BOT_URL")
 
 	return
@@ -23,10 +25,11 @@ func NewServicesConfigFromEnv() (config Services) {
 
 func (config Services) String() string {
 	return fmt.Sprintf(
-		"\n---\nService grid configuration:\n\tControl center URL: %s\n\tCoordinator URL: %s\n\tCollector URL: %s\n\tTelegram admin bot URL: %s",
+		"\n---\nService grid configuration:\n\tControl center URL: %s\n\tCoordinator URL: %s\n\tCollector URL: %s\n\tAuth URL: %s\n\tTelegram admin bot URL: %s",
 		config.ControlCenterURL,
 		config.CoordinatorURL,
 		config.CollectorURL,
+		config.AuthURL,
 		config.TelegramAdminBotURL,
 	)
 }
